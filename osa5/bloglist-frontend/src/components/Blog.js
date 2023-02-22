7987fe1fadb2fdd5d90591942219404c4cdb02f0
rudolf <rudolf.mard@gmail.com>
2023-02-22 12:20:20 +0200

    Tehtävä 5.7: blogilistan frontend step7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,36 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const [expand, setExpand] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleExpand = () => {
+    setExpand(!expand)
+  }
+
+  if (expand){
+    return(
+      <div style={blogStyle}>
+        {blog.title} {blog.author}<button style={{marginLeft: 5}} onClick={toggleExpand}>hide</button><br/>
+        {blog.url}<br/>
+        likes {blog.likes}<button style={{marginLeft: 5}}>like</button><br/>
+        {blog.user.name}
+      </div>  
+    )
+  }
+  return(
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button style={{marginLeft: 5}} onClick={toggleExpand}>view</button>
+    </div>  
+  )
+  }
 
 export default Blog
