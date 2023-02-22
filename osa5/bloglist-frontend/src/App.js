eb816a666e6486599816291b7629675cc8f7017a
rudolf <rudolf.mard@gmail.com>
2023-02-22 13:20:01 +0200

    Tehtävä 5.10: blogilistan frontend step10

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -130,8 +130,9 @@ const App = () => {
         <BlogForm createBlog={addBlog}/>
       </Togglable>
 
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} addLike={addLike}/>
+      {blogs
+        .sort((a,b) => b.likes-a.likes)
+        .map(blog => <Blog key={blog.id} blog={blog} addLike={addLike}/>
       )}
     </div>
   )
