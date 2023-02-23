02eee72f5534f93723ecf4eb7b433b200aebb005
rudolf <rudolf.mard@gmail.com>
2023-02-23 13:52:25 +0200

    Tehtävä 5.14: blogilistan testit step2

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders un-togglable content only', async () => {
-  const blog = {
-    user: 'test user',
+  const testBlog = {
+    user: {
+      username: 'test user',
+      name: 'Testi Jannu'
+    },
     likes: 0,
     author: 'test author',
     title: 'test title',
@@ -13,7 +17,7 @@ test('renders un-togglable content only', async () => {
     id: 'test id'
   }
 
-  render(<Blog blog={blog} addLike={null} removeBlog={null} username={'test user'}/>)
+  render(<Blog blog={testBlog} addLike={null} removeBlog={null} username={testBlog.user.username}/>)
 
   const element = screen.getByText('test title test author')
   expect(element).toBeDefined()
@@ -23,4 +27,38 @@ test('renders un-togglable content only', async () => {
 
   const element3 = screen.queryByText(0, { exact: false })
   expect(element3).toBeNull()
+})
+
+test('Clicking the button shows togglable content', async () => {
+  const blog = {
+    user: {
+      username: 'test user',
+      name: 'Testi Jannu'
+    },
+    likes: 10,
+    author: 'test author',
+    title: 'test title',
+    url: 'test url',
+    id: 'test id'
+  }
+
+  const mockHandlerLike = jest.fn()
+  const mockHandlerRemove = jest.fn()
+
+  render(
+    <Blog blog={blog} addLike={mockHandlerLike} removeBlog={mockHandlerRemove} username={'test user'}/>
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const element = screen.getByText('test url', { exact: false })
+  expect(element).toBeDefined()
+
+  const element2 = screen.getByText('Testi Jannu', { exact: false })
+  expect(element2).toBeDefined()
+
+  const element3 = screen.getByText('likes 10', { exact: false })
+  expect(element3).toBeDefined()
 })
