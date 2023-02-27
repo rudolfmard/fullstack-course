3f3007a038838f70a75ed5cc05dfeffb410a15ed
rudolf <rudolf.mard@gmail.com>
2023-02-27 15:31:15 +0200

    Tehtävä 5.18: blogilistan e2e testit step2

--- osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const testUser = {
+      name: 'Poppy Dickings',
+      username: 'pdick',
+      password: 'salasana'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', testUser)
     cy.visit('http://localhost:3000')
   })
 
@@ -8,5 +14,23 @@ describe('Blog app', function() {
     cy.get('#username')
     cy.get('#password')
     cy.get('#login-button')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('pdick')
+      cy.get('#password').type('salasana')
+      cy.get('#login-button').click()
+
+      cy.contains('Poppy Dickings logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('pduck')
+      cy.get('#password').type('1234')
+      cy.get('#login-button').click()
+
+      cy.contains('wrong username or password')
+    })
   })
 })
