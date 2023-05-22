d132ec918bf1223554e889ebf71ead9cd2583f6f
rudolf <rudolf.mard@gmail.com>
2023-05-22 13:34:24 +0300

    Tehtävä 6.9: paremmat anekdootit, step7

--- osa6/redux-anecdotes-main/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes-main/src/components/AnecdoteList.js
@@ -2,7 +2,11 @@ import { useSelector, useDispatch } from "react-redux"
 import { voteAnecdote } from "../reducers/anecdoteReducer"
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => 
+    state.anecdotes.filter((item) => {
+      return item.content.includes(state.filter)
+    })
+  )
   const dispatch = useDispatch()
 
   const vote = (id) => {
